73174f2a120e856d1b6e067235249f8c58e7f618
Dmitri Vassilev <dmitrivassilev@gmail.com>
2017-04-12 14:35:31 -0400

    syntax error fix -- no es6 support

--- app/assets/javascripts/public/mobile_nav.js
+++ app/assets/javascripts/public/mobile_nav.js
@@ -1,7 +1,7 @@
 $(function() {
   var mobileMenuGraphic = document.querySelector('.mobile-menu-graphic');
   var mobileMenuContent = document.querySelector('.mobile-header-content');
-  mobileMenuGraphic.addEventListener('click', (event) => {
+  mobileMenuGraphic.addEventListener('click', function(event) {
     if (mobileMenuContent.classList.contains('active')) {
       mobileMenuContent.classList.remove('active');
     }else{
